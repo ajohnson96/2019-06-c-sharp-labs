861c75f3ab195878e906d3101ab1f232a46e0d7b
Alexander Johnson <AJohnson@spartaglobal.com>
2019-06-24 13:46:27 +0100

    Adding forms apps

---
+++ labs/lab_03_old_forms_app/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_03_old_forms_app
+{
+    public partial class Form1 : Form
+    {
+        static int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Initialise();
+        }
+
+        private void Initialise()
+        {
+            label1.Text = "What is your name?";
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Enjoy your stay " + textBox1.Text + "!";
+            counter ++;
+            label2.Text = counter.ToString();
+
+            if(label2.Text == "7")
+            {
+                label1.Text = "Lucky you! Good work!";
+            }
+            else if (counter >= 8 && counter < 13)
+            {
+                label1.Text = "Alright slow it down...";
+            }
+            else if (counter >= 13 && counter < 30)
+            {
+                label1.Text = "Is your mouse broken?";
+            }
+        }
+    }
+}
---
+++ labs/lab_04_WPF_starter/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab_04_WPF_starter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int counter = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Initialise();
+        }
+
+        void Initialise()
+        {
+            List<string> comboItems = new List<string>() { "One", "Two", "Three" };
+            Combobox01.ItemsSource = comboItems;
+        }
+
+
+        private void Text01_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button01_Click(object sender, RoutedEventArgs e)
+        {
+            counter++;
+            List01.Items.Add($"You have clicked {counter} times");
+
+        }
+
+        private void List01_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Combobox01_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItem = Combobox01.SelectedItem;
+            List01.Items.Add(selectedItem);
+        }
+    }
+}

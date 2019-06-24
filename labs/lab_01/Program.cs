b436b4477a4f837963432fff775499b8f9450b87
Alexander Johnson <AJohnson@spartaglobal.com>
2019-06-24 10:26:22 +0100

    Initial Commit Visual Studio

---
+++ labs/lab_01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lab_01
+{
+    class Program
+    {
+        private static string msg;
+
+        static void Main(string[] args)
+        {
+            msg = "Hello World From Alex!";
+            Console.WriteLine(msg);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ labs/lab_02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello From Alex!");
+            Console.ReadLine();
+        }
+    }
+}
